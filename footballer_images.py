aec8a0e37705fb35375045ec8f091f676b0ae08e
Mohamed EL AITOUNI <76909500+Batman-sys@users.noreply.github.com>
2023-03-29 00:03:59 +0200

    Add files via upload

--- footballer_images.py
+++ footballer_images.py
@@ -6,7 +6,8 @@ import requests
 import io
 from PIL import Image
 import time
-
+import sys
+sys.setrecursionlimit(1000000)
 PATH = "C:/Users/user/Downloads/chromedriver_win32/chromedriver.exe"
 a = webdriver.Chrome(PATH)
 players = pd.read_csv('./player_team.csv')
@@ -18,21 +19,25 @@ def get_images(players, p, a, time_delay):
         time.sleep(time_delay)
     s = p.split()
     team = players[players['Player Name'] == p]['Team'].values
+    
     if team[0] and p:
-        url = "https://www.google.com/search?q=" + p +"+" + team[0] + "+png&gs_lcp=CgNpbWcQAzIHCAAQgAQQEzoHCAAQigUQQzoFCAAQgAQ6BggAEAcQHjoECAAQHjoGCAAQHhATUIsDWKsLYNIMaABwAHgAgAFMiAGzApIBATWYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=TSceZPmwNJ64nsEPkM25oAc&bih=752&biw=767"
+        url = "https://www.google.com/search?q="+ p + " " + team[0] + "+portrait&tbm=isch&hl=fr&tbs=ic:trans&rlz=1C1SQJL_frFR979FR979&sa=X&ved=0CAMQpwVqFwoTCOjP_P7n9v0CFQAAAAAdAAAAABAE&biw=1519&bih=760"
     else:
         return ''
+    
     a.get(url)
-    thumbnails = a.find_elements(By.CLASS_NAME, 'uhHOwf')
+    if p == players['Player Name'].values[0]:
+        time.sleep(5)
+    thumbnails = a.find_elements(By.CLASS_NAME, 'Q4LuWd')
     for img in thumbnails:
         try:
             img.click()
-            time.sleep(time_delay)
+            
         except:
             continue
         images = a.find_elements(By.CLASS_NAME, "n3VNCb")
         for image in images:
-            print(image)
+            print(image.get_attribute('src'))
             if image.get_attribute('src'):
                 return image.get_attribute('src')
             
@@ -41,7 +46,6 @@ def get_images(players, p, a, time_delay):
 l = []
 for i in player_names:
     l.append(get_images(players, i, a, 1))
-    time.sleep(2)
 players.insert(2, "image_url", l, True)
 players.to_csv('C:\\Users\\user\\Desktop\\Projet_Annuel\\player_team&photo.csv', index=False)
 
