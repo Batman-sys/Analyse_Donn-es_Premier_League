fbdb86162d46d059027ae4fdb9bd53704031a1ad
KrmKa <126170061+KrmKa@users.noreply.github.com>
2023-04-01 02:16:08 +0200

    Update match_data.py

--- match_data.py
+++ match_data.py
@@ -17,6 +17,8 @@ import plotly.graph_objs as go
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
+import base64
+import copy as cp
 
 
 
@@ -41,17 +43,17 @@ def xG_Score(df, team1, team2):
     #df = importing_events(team1, team2)
     
     df1 = df[df["Player1 Team"] == team1]
-    fig1 = go.Figure(data=go.Scatter(x=df1['Time'], y=df1['xG Score'], name=team1))
+    fig1 = go.Figure(data=go.Scatter(x=df1['Time']/60, y=df1['xG Score'], name=team1))
     fig1.update_layout(title_text=team1)
     
     df2 = df[df["Player1 Team"] == team2]
-    fig2 = go.Figure(data=go.Scatter(x=df2['Time'], y=df2['xG Score'], name=team2))
+    fig2 = go.Figure(data=go.Scatter(x=df2['Time']/60, y=df2['xG Score'], name=team2))
     fig2.update_layout(title_text=team2)
     
-    return fig1.show() , fig2.show()
+    return fig1 , fig2
     
  
- def Offensiveness(df, team1, team2):
+def Offensiveness(df, team1, team2):
     
     #df = importing_events(team1, team2)
     
@@ -61,12 +63,11 @@ def xG_Score(df, team1, team2):
     df2 = df[df["Player1 Team"] == team2]
     fig2 = px.line(df2, x="Time", y="Offensiveness", title=team2)
 
-    return fig1.show(), fig2.show()
+    return fig1, fig2
     
     
     
 def Nb_Pass_succ(df, team1, team2):    
-    #df = importing_events(team1, team2)
     team_1 = 0
     team_2 = 0
     for (i,j,k) in zip(df["Event Name"], df["Player1 Team"], df["Player1 Team"][1::] ):
@@ -81,7 +82,6 @@ def Nb_Pass_succ(df, team1, team2):
     return team_1, team_2
 
 def Nb_Pass_miss(df, team1, team2):    
-    #df = importing_events(team1, team2)
     team_1 = 0
     team_2 = 0
     for (i,j,k) in zip(df["Event Name"], df["Player1 Team"], df["Player1 Team"][1::] ):
@@ -96,88 +96,49 @@ def Nb_Pass_miss(df, team1, team2):
     return team_1, team_2
 
 def Pass(df, team1, team2):
-    #df = importing_events(team1, team2)
     team_1_pass = len(df[ (df["Event Name"]=="Pass") & (df["Player1 Team"]== team1)])
-    team_1_succ = Nb_Pass_succ(team1, team2)[0]
-    team_1_miss = Nb_Pass_miss(team1, team2)[0]
+    team_1_succ = Nb_Pass_succ(df, team1, team2)[0]
+    team_1_miss = Nb_Pass_miss(df, team1, team2)[0]
     team_2_pass = len(df[ (df["Event Name"]=="Pass") & (df["Player1 Team"]== team2)])
-    team_2_succ = Nb_Pass_succ(team1, team2)[1]
-    team_2_miss = Nb_Pass_miss(team1, team2)[1]
+    team_2_succ = Nb_Pass_succ(df, team1, team2)[1]
+    team_2_miss = Nb_Pass_miss(df, team1, team2)[1]
     return (team_1_pass, team_1_succ, team_1_miss), (team_2_pass, team_2_succ, team_2_miss)
 
 
+  
 def match_stats(df, Team1, Team2, minutes):
-  df = importing_events(Team1, Team2)
+    
+    goal_team1 = len(df[ (df["Time"] < minutes) & ( ((df["Event Name"] == "Goal") & (df["Player1 Team"] == Team1)) | ((df["Event Name"] == "Own Goal") & (df["Player1 Team"] == Team2)))   ])
+    goal_team2 = len(df[ (df["Time"] < minutes) & ( ((df["Event Name"] == "Goal") & (df["Player1 Team"] == Team2)) | ((df["Event Name"] == "Own Goal") & (df["Player1 Team"] == Team1)))   ])
+    shot_team1 = len(df[ (df["Time"] < minutes) & (df["Event Name"].str.contains("Shot")) & (df["Player1 Team"] == Team1)])
+    shot_team2 = len(df[ (df["Time"] < minutes) & (df["Event Name"].str.contains("Shot") ) & (df["Player1 Team"] == Team2 )])
+    redCard_team1 = len(df[ (df["Time"] < minutes) & (df["Event Name"].str.contains("Red Card") ) & (df["Player1 Team"] == Team1 )])
+    redCard_team2 = len(df[ (df["Time"] < minutes) & (df["Event Name"].str.contains("Red Card") ) & (df["Player1 Team"] == Team2 )])
+    yellowCard_team1 = len(df[ (df["Time"] < minutes) & (df["Event Name"].str.contains("Yellow Card") ) & (df["Player1 Team"] == Team1 )])
+    yellowCard_team2 = len(df[ (df["Time"] < minutes) & (df["Event Name"].str.contains("Yellow Card") ) & (df["Player1 Team"] == Team2 )])
+    pass_team1 = len(df[ (df["Time"] < minutes) & (df["Event Name"].str.contains("Pass") ) & (df["Player1 Team"] == Team1 )])
+    pass_team2 = len(df[ (df["Time"] < minutes) & (df["Event Name"].str.contains("Pass") ) & (df["Player1 Team"] == Team2 )])
+    foul_team1 = len(df[ (df["Time"] < minutes) & (df["Event Name"].str.contains("Foul") ) & (df["Player1 Team"] == Team1 )])
+    foul_team2 = len(df[ (df["Time"] < minutes) & (df["Event Name"].str.contains("Foul") ) & (df["Player1 Team"] == Team2 )])
+    offSide_team1 = len(df[ (df["Time"] < minutes) & (df["Event Name"].str.contains("Offside") ) & (df["Player1 Team"] == Team1 )])
+    offSide_team2 = len(df[ (df["Time"] < minutes) & (df["Event Name"].str.contains("Offside") ) & (df["Player1 Team"] == Team2 )])
+    corners_team1 = len(df[ (df["Time"] < minutes) & (df["Event Name"].str.contains("Corner") ) & (df["Player1 Team"] == Team1 )])
+    corners_team2 = len(df[ (df["Time"] < minutes) & (df["Event Name"].str.contains("Corner") ) & (df["Player1 Team"] == Team2 )])
+    
+    total_possession = len(df[(df["Time"] < minutes) & ((df["Player1 Team"] == Team2 ) | (df["Player1 Team"] == Team1 )) ])
+    possession_team1 = len(df[(df["Time"] < minutes) & (df["Player1 Team"] == Team1 )])/total_possession * 100 
+    possession_team2 = len(df[(df["Time"] < minutes) & (df["Player1 Team"] == Team2 )])/total_possession * 100 
 
-  goal_team1 = len(
-    df[(df["Time"] < minutes)
-       & (((df["Event Name"] == "Goal") & (df["Player1 Team"] == Team1)) | (
-         (df["Event Name"] == "Own Goal") & (df["Player1 Team"] == Team2)))])
-  goal_team2 = len(
-    df[(df["Time"] < minutes)
-       & (((df["Event Name"] == "Goal") & (df["Player1 Team"] == Team2)) | (
-         (df["Event Name"] == "Own Goal") & (df["Player1 Team"] == Team1)))])
-  shot_team1 = len(
-    df[(df["Time"] < minutes) & (df["Event Name"].str.contains("Shot")) &
-       (df["Player1 Team"] == Team1)])
-  shot_team2 = len(
-    df[(df["Time"] < minutes) & (df["Event Name"].str.contains("Shot")) &
-       (df["Player1 Team"] == Team2)])
-  redCard_team1 = len(
-    df[(df["Time"] < minutes) & (df["Event Name"].str.contains("Red Card")) &
-       (df["Player1 Team"] == Team1)])
-  redCard_team2 = len(
-    df[(df["Time"] < minutes) & (df["Event Name"].str.contains("Red Card")) &
-       (df["Player1 Team"] == Team2)])
-  yellowCard_team1 = len(
-    df[(df["Time"] < minutes) & (df["Event Name"].str.contains("Yellow Card"))
-       & (df["Player1 Team"] == Team1)])
-  yellowCard_team2 = len(
-    df[(df["Time"] < minutes) & (df["Event Name"].str.contains("Yellow Card"))
-       & (df["Player1 Team"] == Team2)])
-  pass_team1 = len(
-    df[(df["Time"] < minutes) & (df["Event Name"].str.contains("Pass")) &
-       (df["Player1 Team"] == Team1)])
-  pass_team2 = len(
-    df[(df["Time"] < minutes) & (df["Event Name"].str.contains("Pass")) &
-       (df["Player1 Team"] == Team2)])
-  foul_team1 = len(
-    df[(df["Time"] < minutes) & (df["Event Name"].str.contains("Foul")) &
-       (df["Player1 Team"] == Team1)])
-  foul_team2 = len(
-    df[(df["Time"] < minutes) & (df["Event Name"].str.contains("Foul")) &
-       (df["Player1 Team"] == Team2)])
-  offSide_team1 = len(
-    df[(df["Time"] < minutes) & (df["Event Name"].str.contains("Offside")) &
-       (df["Player1 Team"] == Team1)])
-  offSide_team2 = len(
-    df[(df["Time"] < minutes) & (df["Event Name"].str.contains("Offside")) &
-       (df["Player1 Team"] == Team2)])
-  corners_team1 = len(
-    df[(df["Time"] < minutes) & (df["Event Name"].str.contains("Corner")) &
-       (df["Player1 Team"] == Team1)])
-  corners_team2 = len(
-    df[(df["Time"] < minutes) & (df["Event Name"].str.contains("Corner")) &
-       (df["Player1 Team"] == Team2)])
-
-  total_possession = len(
-    df[(df["Time"] < minutes)
-       & ((df["Player1 Team"] == Team2) | (df["Player1 Team"] == Team1))])
-  possession_team1 = len(
-    df[(df["Time"] < minutes)
-       & (df["Player1 Team"] == Team1)]) / total_possession * 100
-  possession_team2 = len(
-    df[(df["Time"] < minutes)
-       & (df["Player1 Team"] == Team2)]) / total_possession * 100
-
-  return goal_team1, goal_team2, shot_team1, shot_team2, redCard_team1, redCard_team2, yellowCard_team1, yellowCard_team2, pass_team1, pass_team2, foul_team1, foul_team2, offSide_team1, offSide_team2, corners_team1, corners_team2, possession_team1, possession_team2
+    return goal_team1, goal_team2, shot_team1,shot_team2,redCard_team1, redCard_team2, yellowCard_team1, yellowCard_team2, pass_team1, pass_team2,foul_team1, foul_team2,offSide_team1, offSide_team2,corners_team1, corners_team2, possession_team1, possession_team2
 
 
 # partie du terrain correspondant à l'équipe adverse découpé en 3 parties ( sur la longueur )
 # cotés utilisés.
 
 def sides_used(df, team1, team2, minutes):
-    #df = importing_events(team1, team2)
+    df.X = (-1)**df.Half*df.X
+    df.Y = (-1)**df.Half*df.Y  
+    
     total1 = len(df[ ( df["Player1 Team"] == team1 ) & (df["Time"] < minutes) & (df["X"] > 0) ])
     total2 = len(df[ ( df["Player1 Team"] == team2 ) & (df["Time"] < minutes) & (df["X"] < 0) ])
     #print(total1,total2)
@@ -197,37 +158,67 @@ def sides_used(df, team1, team2, minutes):
     
     return (left_side1, middle1, right_side1), (left_side2, middle2, right_side2)
             
-            
-            
+
+    
+def plot_sides_used(df, team1, team2, minutes):
+    # Obtenir les données des côtés utilisés pour chaque équipe
+    side1, side2 = sides_used(df, team1, team2, minutes)
+
+    # Créer les traces pour chaque équipe
+    trace1 = go.Bar(x=['Côté gauche', 'Milieu', 'Côté droit'], y = side1, name = team1)
+    trace2 = go.Bar(x=['Côté gauche', 'Milieu', 'Côté droit'], y = side2, name = team2)
+
+    # Créer la figure et l'afficher
+    fig = go.Figure(data=[trace1, trace2])
+    fig.update_layout(title=f'Côtés utilisés par {team1} et {team2} pendant les premières {minutes/60} minutes ', barmode='group')
+    return fig
+  
+  
+  
+  
+  
 # partie du terrain correspondant à l'équipe adverse découpé en 3 parties ( sur la longueur )
 
 def Directions_of_shots(df, team1, team2, minutes):
-    #df = importing_events(team1, team2)
-    total1 = len(df[ ( df["Player1 Team"] == team1 ) & (df["Time"] < minutes) & (df["X"] > 0) & ((df["Event Name"] == "Shot") | (df["Event Name"] == "Direct Free Kick Cross"))])
-    total2 = len(df[ ( df["Player1 Team"] == team2 ) & (df["Time"] < minutes) & (df["X"] < 0) & ((df["Event Name"] == "Shot") | (df["Event Name"] == "Direct Free Kick Cross"))])
-    print(total1, total2)
+   
+    dfr = df.copy()
+    dfr.X = (-1)**dfr.Half*dfr.X
+    dfr.Y = (-1)**dfr.Half*dfr.Y  
     
-    left_side1 = len(df[ ( df["Player1 Team"] == team1 ) & (df["Time"] < minutes) & (df["X"] > 0) & (df["Y"] > 12) & ((df["Event Name"] == "Shot") | (df["Event Name"] == "Direct Free Kick Cross")) ])
+    total1 = len(dfr[ ( dfr["Player1 Team"] == team1 ) & (dfr["Time"] < minutes) & ((dfr["Event Name"] == "Shot") | (dfr["Event Name"] == "Direct Free Kick Cross"))])
+    total2 = len(dfr[ ( dfr["Player1 Team"] == team2 ) & (dfr["Time"] < minutes) & ((dfr["Event Name"] == "Shot") | (dfr["Event Name"] == "Direct Free Kick Cross"))])
+    if total1 == 0 and total2 ==0:
+        total1 = 1
+        total2 =1
+    if total1 == 0:
+        total1 = 1
+    if total2 == 0:
+        total2 = 1
+  
+    left_side1 = len(dfr[ ( dfr["Player1 Team"] == team1 ) & (dfr["Time"] < minutes) & (dfr["Y"] > 12) & ((dfr["Event Name"] == "Shot") | (dfr["Event Name"] == "Direct Free Kick Cross")) ])
     left_side1 = left_side1 / total1
-    middle1 = len(df[ ( df["Player1 Team"] == team1 ) & (df["Time"] < minutes) & (df["X"] > 0) & (df["Y"] < 12) & (df["Y"] > -12) & ((df["Event Name"] == "Shot") | (df["Event Name"] == "Direct Free Kick Cross"))])
+    middle1 = len(dfr[ ( dfr["Player1 Team"] == team1 ) & (dfr["Time"] < minutes)& (dfr["Y"] < 12) & (dfr["Y"] > -12) & ((dfr["Event Name"] == "Shot") | (dfr["Event Name"] == "Direct Free Kick Cross"))])
     middle1 = middle1 / total1
-    right_side1 = len(df[ ( df["Player1 Team"] == team1 ) & (df["Time"] < minutes) & (df["X"] > 0) & (df["Y"] < -12) & ((df["Event Name"] == "Shot") | (df["Event Name"] == "Direct Free Kick Cross")) ])
+    right_side1 = len(dfr[ ( dfr["Player1 Team"] == team1 ) & (dfr["Time"] < minutes) & (dfr["Y"] < -12) & ((dfr["Event Name"] == "Shot") | (dfr["Event Name"] == "Direct Free Kick Cross")) ])
     right_side1 = right_side1 / total1
     
-    left_side2 = len(df[ ( df["Player1 Team"] == team2 ) & (df["Time"] < minutes) & (df["X"] < 0) & (df["Y"] < -12)  & ((df["Event Name"] == "Shot") | (df["Event Name"] == "Direct Free Kick Cross"))])
-    left_side2 = left_side1 / total2
-    middle2 = len(df[ ( df["Player1 Team"] == team2 ) & (df["Time"] < minutes) & (df["X"] < 0) & (df["Y"] < 12) & (df["Y"] > -12) & ((df["Event Name"] == "Shot") | (df["Event Name"] == "Direct Free Kick Cross"))])
+    left_side2 = len(dfr[ ( dfr["Player1 Team"] == team2 ) & (dfr["Time"] < minutes)  & (dfr["Y"] <= -12)  & ((dfr["Event Name"] == "Shot") | (dfr["Event Name"] == "Direct Free Kick Cross"))])
+    left_side2 = left_side2 / total2
+    
+    middle2 = len(dfr[ ( dfr["Player1 Team"] == team2 ) & (dfr["Time"] < minutes)  & (dfr["Y"] < 12) & (dfr["Y"] > -12) & ((dfr["Event Name"] == "Shot") | (dfr["Event Name"] == "Direct Free Kick Cross"))])
     middle2 = middle2 / total2
-    right_side2 = len(df[ ( df["Player1 Team"] == team2 ) & (df["Time"] < minutes) & (df["X"] < 0) & (df["Y"] > 12)  & ((df["Event Name"] == "Shot") | (df["Event Name"] == "Direct Free Kick Cross"))])
+    
+    right_side2 = len(dfr[ ( dfr["Player1 Team"] == team2 ) & (dfr["Time"] < minutes)  & (dfr["Y"] > 11)  & ((dfr["Event Name"] == "Shot") | (dfr["Event Name"] == "Direct Free Kick Cross"))])
     right_side2 = right_side2 / total2
+    
     
     return (left_side1, middle1, right_side1), (left_side2, middle2, right_side2)
     
     
     
-
-# partie du terrain correspondant à l'équipe adverse 
     
+# partie du terrain correspondant à l'équipe adverse 
+# a corriger
 def Shooting_zones(df, team1, team2, minutes):
     #df = importing_events(team1, team2)
     total1 = len(df[ ( df["Player1 Team"] == team1 ) & (df["Time"] < minutes) & (df["X"] > 0) & ((df["Event Name"] == "Shot") | (df["Event Name"] == "Direct Free Kick Cross"))])
@@ -263,31 +254,50 @@ def Shooting_zones(df, team1, team2, minutes):
 # partie du terrain découpé en 3 parties (largeur) : Tiers_gauche, Milieu, Tiers_droit
 
 def Action_areas(df, team1, team2, minutes):
-    #df = importing_events(team1, team2)
-    total = len(df[(df["Time"] < minutes)])
-    Tiers_gauche = len(df[ ( ( df["Player1 Team"] == team1 ) | ( df["Player1 Team"] == team2 ) ) & (df["Time"] < minutes) & (df["X"] <= -18.4) ])
+    dfr = df.copy()
+    dfr.X = (-1)**dfr.Half*dfr.X
+    dfr.Y = (-1)**dfr.Half*dfr.Y  
+    total = len(dfr[(dfr["Time"] < minutes)])
+    
+    Tiers_gauche = len(dfr[ ( ( dfr["Player1 Team"] == team1 ) | ( dfr["Player1 Team"] == team2 ) ) & (dfr["Time"] < minutes) & (dfr["X"] <= -18.4) ])
     Tiers_gauche = (Tiers_gauche / total) * 100
-    Milieu = len(df[ ( ( df["Player1 Team"] == team1 ) | ( df["Player1 Team"] == team2 ) ) & (df["Time"] < minutes) & (df["X"] >= -18.4) & (df["X"] <= 18.4) ])
+    Milieu = len(df[ ( ( dfr["Player1 Team"] == team1 ) | ( dfr["Player1 Team"] == team2 ) ) & (dfr["Time"] < minutes) & (dfr["X"] >= -18.4) & (dfr["X"] <= 18.4) ])
     Milieu = (Milieu / total) * 100
-    Tiers_droit = len(df[ ( ( df["Player1 Team"] == team1 ) | ( df["Player1 Team"] == team2 ) ) & (df["Time"] < minutes) & (df["X"] >= 18.4) ])
+    Tiers_droit = len(dfr[ ( ( dfr["Player1 Team"] == team1 ) | ( dfr["Player1 Team"] == team2 ) ) & (dfr["Time"] < minutes) & (dfr["X"] >= 18.4) ])
     Tiers_droit = (Tiers_droit / total) * 100
 
-    return ( str(round(Tiers_gauche)) + " %",str(round(Milieu)) + "%", str(round(Tiers_droit)) + "%" )
+    return [ [math.ceil(Tiers_gauche) ,math.ceil(Milieu) ,math.ceil(Tiers_droit)] ]
     
-    
-def plot_cotes_utilises(df, team1, team2, minutes):
-    # Obtenir les données des côtés utilisés pour chaque équipe
-    side1, side2 = sides_used(df, team1, team2, minutes)
 
-    # Créer les traces pour chaque équipe
-    trace1 = go.Bar(x=['Côté gauche', 'Milieu', 'Côté droit'], y = side1, name = team1)
-    trace2 = go.Bar(x=['Côté gauche', 'Milieu', 'Côté droit'], y = side2, name = team2)
-
-    # Créer la figure et l'afficher
-    fig = go.Figure(data=[trace1, trace2])
-    fig.update_layout(title=f'Côtés utilisés par {team1} et {team2} pendant les premières {minutes} minutes', barmode='group')
+def heatmap_action_venues(df, team1, team2, minutes):
+    dfr = cp.copy(df)
+    minutes *= 60
+    title = str(team2) + " touch locations"
+    dfr['X']+= 52
+    dfr['Y']+= 34
+    data = Action_areas(df, team1, team2, minutes)
+    # Création de la figure avec la carte de chaleur et l'image de terrain
+    dimensions = PitchDimensions()
+    fig = make_pitch_figure(
+    dimensions,
+    pitch_background=SingleColourBackground("#81B622"),
+    )
+        
+    fig = add_heatmap(fig, np.array(data))
+    fig.update_layout(
+    title="Action Venues ",
+    xaxis_title="X Axis Title",
+    yaxis_title="Y Axis Title",
+    legend_title="Legend Title",
+    font=dict(
+        family="Courier New, monospace",
+        size=18,
+        color="RebeccaPurple"
+        )
+    )
     return fig
-    
+
+
 
 def draw_team_pass(df_events, team, minutes):
         title = str(team) + " pass locations"
@@ -348,10 +358,60 @@ def draw_team_pass(df_events, team, minutes):
 
         return fig
         
+
+def plot_xG_goal(df, team1, team2, minutes):
+    #df = importing_events(team1, team2)
+    print(df.columns)
+    # Obtenir les données des côtés utilisés pour chaque équipe
+    #side1, side2 = sides_used(df, team1, team2, minutes)
+    x1 = df[(df["Player1 Team"] == team1) & (df["Time"] <= minutes)]["xG Score"].values.sum(), df[ (df["Player1 Team"] == team2) & (df["Time"] <= minutes)]["xG Score"].values.sum()
+    x2 = len(df[ (df["Event Name"] == "Goal") & (df["Player1 Team"] == team1) & (df["Time"] <= minutes)]["Event Name"]), len(df[ (df["Event Name"] == "Goal") & (df["Player1 Team"] == team2) & (df["Time"] <= minutes)]["Event Name"])
+    print(x1)
+    print(df.columns)
+
+    # Créer les traces pour chaque équipe
+    fig = go.Figure(data=[
+        go.Bar(x=[team1, team2], y = x1, name = 'xG'),
+        go.Bar(x=[team1, team2], y = x2, name = 'Goal'),
         
+    ])
+    # Change the bar mode
+    fig.update_layout(barmode='group')
+
+    return fig
         
- 
     
+    
+def touches(df, team1, team2, minute):
+        minute *= 60
+        title = str(team2) + " touch locations"
+        
+        dfr = cp.copy(df)
+       # minutes *= 60
+       # title = str(team2) + " touch locations"
+        dfr['X']+= 52
+        dfr['Y']+= 34
+        team_pos = dfr[(dfr['Time'] <= minute) & (dfr['Player1 Team'] == team1)]
+        team_pos1 = dfr[(dfr['Time'] <= minute) & (dfr['Player1 Team'] == team2)]
+        x=team_pos['X'].values
+        y=team_pos['Y'].values
+        
+        x1 = team_pos1['X'].values
+        y1=team_pos1['Y'].values
+        
+        
+        dimensions = PitchDimensions()
+        fig = make_pitch_figure(
+        dimensions,
+        pitch_background=SingleColourBackground("#81B622"),
+        )
+        fig.add_trace(go.Scatter(x = x, y = y, mode='markers', marker_color = 'blue'))
+        fig.add_trace(go.Scatter(x = x1, y = y1, mode='markers', marker_color = 'orange'))
+        
+
+        return fig
+      
+      
 def heatmap(df, team1, team2, minutes):
     df = df.copy()
     df["X"] += 55
@@ -373,6 +433,5 @@ def heatmap(df, team1, team2, minutes):
     ])
     fig = add_heatmap(fig, data)
     return fig.show()
-    
-    
-n
+  
+  
