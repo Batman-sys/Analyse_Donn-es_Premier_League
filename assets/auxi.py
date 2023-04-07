730295bd2e6d87221a54981117cb1cd7cd4fa834
Mohamed EL AITOUNI <76909500+Batman-sys@users.noreply.github.com>
2023-04-07 16:47:54 +0200

    Delete assets directory

--- assets/auxi.py
+++
@@ -1,7 +0,0 @@
-def eventOfPlayerPerMatch_auxi(p,event,df_players,df_events):
-    d={}
-    data=df_players.loc[df_players["Player Name"]== p]['Match Name']
-    for i in data.unique():
-        data2=df_events.loc[(df_events["Player1 Name"]== p)& (df_events['Match Name']==i)&(df_events['Event Name']==event)]
-        d[i]=len(data2)
-    return d
